099c27993b019e1a56a0269b95358d780bb11a8e
naseos2 <119030276+naseos2@users.noreply.github.com>
2024-05-08 14:27:24 +0900

    BattleMenu

--- midtermProject/Assets/Scripts/PlayerBattle.cs
+++ midtermProject/Assets/Scripts/PlayerBattle.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum BattleState
+{
+    Idle,
+    Hit,
+    Magic,
+    Item,
+    Run,
+}
+
 public class PlayerBattle : MonoBehaviour
 {
+    public BattleState BattleState;
+
     public float atbTime;
     private float nowTime;
 
     public Slider atbSlider;
+    public GameObject battleGUI;
 
     private void Start()
     {
+        BattleState = BattleState.Idle;
         nowTime = 0f;
     }
 
@@ -22,8 +35,21 @@ public class PlayerBattle : MonoBehaviour
 
         if (nowTime >= atbTime)
         {
+            BattlePanel();
+
+        }
+    }
+
+    private void BattlePanel()
+    {
+        battleGUI.SetActive(true);
+
+        if (Input.anyKeyDown)
+        {
             Debug.Log("Player's Attack!");
+            battleGUI.SetActive(false);
             nowTime = 0f;
         }
+
     }
 }
