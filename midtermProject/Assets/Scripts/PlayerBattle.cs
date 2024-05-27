3bc2e869a0ea8dc1a0baefc677b71319aacdf0df
naseos2 <119030276+naseos2@users.noreply.github.com>
2024-05-27 12:22:02 +0900

    Keybord_UI/Magic

--- midtermProject/Assets/Scripts/PlayerBattle.cs
+++ midtermProject/Assets/Scripts/PlayerBattle.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public enum BattleState
 {
@@ -28,11 +29,16 @@ public class PlayerBattle : MonoBehaviour
 
     public GameObject menu;
     public GameObject battleMenu;
+    private bool isMenu;
 
     private GameObject monster;
 
     public Animator transition;
     public float transitionTime = 1f;
+
+    public Button battleBtn;
+    public Button attackBtn;
+
 
     private void Start()
     {
@@ -41,6 +47,7 @@ public class PlayerBattle : MonoBehaviour
         BattleState = BattleState.Idle;
         nowTime = 0f;
         hpSlider.value = nowHp / maxHp;
+
     }
 
     private void Update()
@@ -54,25 +61,45 @@ public class PlayerBattle : MonoBehaviour
         atbSlider.value = nowTime / atbTime;
 
 
-        if (nowTime >= atbTime)
+        if (nowTime >= atbTime && isMenu == false)
         {
             BattlePanel();
             BattleState = BattleState.Idle;
         }
+
+        if (menu.activeSelf)
+        {
+           if (Input.GetKeyDown(KeyCode.Space))
+           {
+               battleBtn.Select();
+           }
+        }
+
+        else if(battleMenu.activeSelf)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                attackBtn.Select();
+            }
+        }
+
     }
 
     private void BattlePanel()
     {
         menu.SetActive(true);
+        isMenu = true;
     }
 
     public void BattleMenu()
     {
+        menu.SetActive(false);
         battleMenu.SetActive(true);
     }
 
     public void Back()
     {
+        menu.SetActive(true);
         battleMenu.SetActive(false);
     }
 
@@ -81,11 +108,26 @@ public class PlayerBattle : MonoBehaviour
         BattleState = BattleState.Hit;
         Debug.Log("Player's Attack!");
         menu.SetActive(false);
+        isMenu = false;
         battleMenu.SetActive(false);
         nowTime = 0f;
 
         Monster m = monster.GetComponent<Monster>();
         m.nowHp -= 20f;
+        m.hpSlider.value = m.nowHp / m.maxHp;
+    }
+
+    public void Magic()
+    {
+        BattleState = BattleState.Magic;
+        Debug.Log("Player's Magic!");
+        menu.SetActive(false);
+        isMenu = false;
+        battleMenu.SetActive(false);
+        nowTime = 0f;
+
+        Monster m = monster.GetComponent<Monster>();
+        m.nowHp -= 30f;
         m.hpSlider.value = m.nowHp / m.maxHp;
     }
 
